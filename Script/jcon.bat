67c21812f654110184d9318c9e530ab1366268ea
rohanprashantpatil <rohanprashantpatil@gmail.com>
2016-01-04 13:28:46 +0530

    Update jcon.bat

--- Script/jcon.bat
+++ Script/jcon.bat
@@ -17,26 +17,36 @@ IF NOT %name% EQU jtmp cd Jcon
 echo cd /D %dire%>>%name%.bat
 echo COPY %dire%\Script\vbs tmp.bat>>%name%.bat
 
+set connection=null
 set /P connection=Connection: 
-echo COPY tmp.bat + %dire%\connection\%connection% + %dire%\Script\jconnect tmp.bat>>%name%.bat
+IF %connection% EQU null goto loop
+
+echo COPY tmp.bat + %dire%\Script\empty + %dire%\connection\%connection% + %dire%\Script\jconnect tmp.bat>>%name%.bat
 
 :loop
 set /P type=File Type: 
 
+IF %type% EQU jcon goto jcon
 IF %type% EQU connection goto connection
 IF %type% EQU endfile goto end
 
 set /P file=File Name: 
 
-echo COPY tmp.bat + %dire%\%type%\%file% tmp.bat>>%name%.bat
+echo COPY tmp.bat  + %dire%\Script\empty + %dire%\%type%\%file% tmp.bat>>%name%.bat
 
 goto loop
+
+:jcon
+
+set /P file=Jcon Name: 
+
+COPY %name%.bat + %dire%\Script\empty + %dire%\Jcon\%file%.bat %name%.bat
 
 :connection
 
 set /P connection=Connection: 
 
-echo COPY tmp.bat + %dire%\connection\%connection% + %dire%\Script\jcon tmp.bat>>%name%.bat
+echo COPY tmp.bat + %dire%\Script\empty + %dire%\connection\%connection% + %dire%\Script\jcon tmp.bat>>%name%.bat
 
 :end
 
